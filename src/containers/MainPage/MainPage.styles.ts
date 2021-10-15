ff0c8dd8e963d3158a296b68ab739f7b999fc356
vladhuk <vladhookovskiy@gmail.com>
2021-10-16 02:17:12 +0300

    - fix styles;

--- src/containers/MainPage/MainPage.styles.ts
+++ src/containers/MainPage/MainPage.styles.ts
@@ -2,15 +2,15 @@ import { makeStyles } from 'utils/make-styles';
 
 export const styles = makeStyles({
   tabBar: {
-    backgroundColor: 'primary.light',
-    borderTopRightRadius: 4,
+    bgcolor: 'primary.light',
     borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
   },
   tabPanel: {
     borderTopLeftRadius: 0,
     borderTopRightRadius: 0,
-    padding: '1rem',
-    backgroundColor: 'background.paper',
+    padding: 5,
+    bgcolor: 'background.paper',
     height: '80vh',
   },
 });
